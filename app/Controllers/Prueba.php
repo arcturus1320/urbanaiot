0ce2e549bbf231acc14fc06ac63e8400e4072eff
pimentelr <apimentelr@gmail.com>
2021-01-25 18:31:00 +0000

    primer Commit

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,19 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		echo "esto es la funcion index";
+	}
+
+  public function test(){
+		echo "esto es una test";
+	}
+
+  public function test2(){
+		echo "esto es una test2";
+	}
+	//--------------------------------------------------------------------
+
+}
